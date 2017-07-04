24b1687a5f6f4e6088efcbf2c98ab8ad5234794f
razvan <tripon.razvan@gmail.com>
2017-07-04 11:18:40 +0300

    f

--- src/app/services/dataservice.service.ts
+++ src/app/services/dataservice.service.ts
@@ -1,11 +1,15 @@
+import {Injectable} from '@angular/core';
+import { LoggingService } from './logging.service';
+@Injectable()
 export class DataserviceService {
   public accounts: { name: string, status: string }[] = [
     { name: 'Master Account', status: 'active' },
     { name: 'Testaccount', status: 'inactive' },
     { name: 'Hidden Account', status: 'unknown' }
   ];
-  constructor() { }
+  constructor(private loggin: LoggingService) { }
   onAccountAdded(newAccount: { name: string, status: string }) {
+    this.loggin.writeLog('a scris ba');
     this.accounts.push(newAccount);
   }
   onStatusChanged(updateInfo: { id: number, newStatus: string }) {
