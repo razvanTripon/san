5ddf3d10f95ed101966010c27b0bae9c8e426b13
razvan <tripon.razvan@gmail.com>
2017-08-01 09:04:40 +0300

    d

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,9 +7,11 @@ import { ServersService } from './servers.service';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  private servers: {id: number, name: string, status: string}[] = [];
+  servers: { id: number, name: string, status: string }[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService) {
+    console.log('d');
+  }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
