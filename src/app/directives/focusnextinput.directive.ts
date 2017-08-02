8f2c4805a3fd0e45513af47f8c3527598db4191f
razvan <tripon.razvan@gmail.com>
2017-08-02 12:11:20 +0300

    d

--- src/app/directives/focusnextinput.directive.ts
+++ src/app/directives/focusnextinput.directive.ts
@@ -1,23 +1,20 @@
-import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+import { Directive, HostListener } from '@angular/core';
 @Directive({
     selector: '[keyNav]'
 })
 export class OnReturnDirective {
-    private el: ElementRef;
-    @Input() onReturn: string;
-    constructor(private _el: ElementRef) {
-        this.el = this._el;
-    }
     @HostListener('keydown', ['$event']) onKeyDown(e) {
         if ((e.which === 13 || e.keyCode === 13)) {
             let control = e.target.nextElementSibling;
             e.preventDefault();
             while (true) {
                 if (control) {
-                    if ((!control.hidden) && control.hasAttribute('keyNav') &&
+                    if ((!control.hidden) &&
+                        control.hasAttribute('keyNav') &&
                         (control.nodeName === 'INPUT' ||
                             control.nodeName === 'SELECT' ||
-                            control.nodeName === 'TEXTAREA')) {
+                            control.nodeName === 'TEXTAREA')
+                    ) {
                         control.focus();
                         if (control.nodeName !== 'SELECT') {
                             control.select();
@@ -27,7 +24,7 @@ export class OnReturnDirective {
                         control = control.nextElementSibling;
                     }
                 } else {
- //                   console.log('gata ba nu a mai gasit elemente pe care sa calce');
+                    //                   console.log('gata ba nu a mai gasit elemente pe care sa calce');
                     return;
                 }
             }
